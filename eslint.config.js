400dfb8389658c7136f032a4230f6c5520f8f8e2
atharvanvknklg <atharvakute55@gmail.com>
2026-09-12 16:36:37 +0530

    Added Calci app eslint code replaced

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
 const js = require("@eslint/js");
 const globals = require("globals");
 
-export default [
-  js.configs.recommended,
+module.exports = [
+    js.configs.recommended,
 
-  {
-    files: ["src/**/*.js"],
-    languageOptions: {
-      globals: globals.node,
+    {
+        files: ["src/**/*.js"],
+        languageOptions: {
+            globals: globals.node
+        }
     },
-  },
 
-  {
-    files: ["tests/**/*.js"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-  },
+    {
+        files: ["tests/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest
+            }
+        }
+    }
 ];
